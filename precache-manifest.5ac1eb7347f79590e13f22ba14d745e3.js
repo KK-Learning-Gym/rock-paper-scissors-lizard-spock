16d3621dd1d1bf8f040abc0a5f3a5f47a3aeb9a6
tinfoil-knight <kunal99kundu@gmail.com>
2020-04-24 21:36:15 +0530

    Updates

---
+++ precache-manifest.5ac1eb7347f79590e13f22ba14d745e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c44757dd990d2a7533e8f77c04183b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36890b839647851cbe19",
+    "url": "./static/css/main.675929d7.chunk.css"
+  },
+  {
+    "revision": "6d82122f08fcb0cadadf",
+    "url": "./static/js/2.7ec90cad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7ec90cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36890b839647851cbe19",
+    "url": "./static/js/main.5823b257.chunk.js"
+  },
+  {
+    "revision": "b943aa5d8fb75699132e",
+    "url": "./static/js/runtime-main.e494e184.js"
+  }
+]);
